6ed047670a1ff4ebd3f476531bf6ec252171b558
pd-karoly-harsanyi <karoly.harsanyi@paralleldomain.com>
2024-07-24 14:24:55 +0200

    add init change setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ with open("requirements.txt") as f:
 setup(
     name="metric_depth",
     version="0.0.1",
-    packages=find_packages(exclude=["assets", "depth_anything_v2"]),
     python_requires=">=3.9",
     install_requires=requirements,
     include_package_data=True,
